f1dc6c19de2eac14e1b597ca5d3f417c2c8bb016
Pankaj KrishnaVanshaj <137090011+PankajKrishnaVanshaj@users.noreply.github.com>
2024-10-24 20:10:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
         </div>
       </header>
 
-      <div className="relative overflow-hidden before:absolute before:top-0 before:left-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')] before:bg-no-repeat before:bg-cover before:transform before:-translate-x-1/2">
+      <div >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
           <div className="flex justify-center">
             <Link
